309b65f8236ec3ecab75823559e012e8d19172ee
Tarkovski7 <trkbgr1618@gmail.com>
2024-01-04 18:03:20 +0300

    takim rep yapıldı

--- Entities/Takim.cs
+++ Entities/Takim.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FutbolcuKartlariMVC.Models.Entities;
 
 namespace FutbolcuKartlariMVC.Entities
 {
-    public class Takim
+    public class Takim : BaseEntity
     {
         public int ID { get; set; }
         public string? TakimAdi { get; set; }
---
+++ Repositories/Abstract/ITakimRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FutbolcuKartlariMVC.Entities;
+
+namespace FutbolcuKartlariMVC.Repositories.Abstract;
+
+public interface ITakimRepository : IRepository<Takim>
+{
+    
+}
